d6e07274cf10f6d7c8bc495779cdcf3948bd502d
idoodi <idoodi@ya.ru>
2022-01-25 14:57:51 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './styles.css';
-// import {createStore} from './createStore'
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { stateMaker } from './stateMaker';
-import {ASYNC_INCR, INCR, DECR, WHATEVER, THEME,} from './dispArgs';
+import { ASYNC_INCR, INCR, DECR, WHATEVER, THEME, } from './dispArgs';
 
 const countElem = document.getElementById('counter');
 const addElem = document.getElementById('add');
@@ -12,14 +11,14 @@ const subElem = document.getElementById('sub');
 const asynElem = document.getElementById('async');
 const themeElem = document.getElementById('theme');
 
-// создает стейт, где есть стейт-значение и массив стейт-функций
+// createStore создает стейт, где есть стейт-значение и массив стейт-функций
 // под капотом данные стейта - это данные из замыкания функции createStore
 // applyMiddleware передает функционал в dispatch
 // thunk чтобы работал асинронный вызов
 // logger чтобы данные выводились в консоль
 const store = createStore(stateMaker, applyMiddleware(thunk, logger));
 
-// getState просто возвразает текущее стейт-значение
+// getState просто возвращает текущее стейт-значение
 const rerenderByState = () => {
   countElem.textContent = store.getState().counterState;
   document.body.className = store.getState().themeState.value;
@@ -29,11 +28,11 @@ const rerenderByState = () => {
 store.subscribe(rerenderByState);
 
 // dispatch :
-// 1)запускает все функции стейта
-// 2)изменяет стейт под действием входного аргумента
-// стейт можно увидеть только после первого dispatch
-store.dispatch(WHATEVER);
+// 1)изменяет стейт под действием входного аргумента
+// 2)запускает все функции стейта
 addElem.addEventListener('click', () => store.dispatch(INCR));
 subElem.addEventListener('click', () => store.dispatch(DECR));
 asynElem.addEventListener('click', () => store.dispatch(ASYNC_INCR));
 themeElem.addEventListener('click', () => store.dispatch(THEME));
+// стейт можно увидеть только после первого dispatch
+store.dispatch(WHATEVER);
